955dd237cbc4c78d13fe7cbb63e90fb76479dbc3
Paweł Hoła <phola85@wp.pl>
2019-09-25 23:39:36 +0200

    Add loop for each tag and HTML of all links into the tags wrapper

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ const titleClickHandler = function(){
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
-  optArticleTagsSelector = 'post-tags .list';
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){
 
@@ -113,15 +113,16 @@ function generateTags() {
 
   const articles = document.querySelectorAll(optArticleSelector);
   console.log('articlesTags: ', articles);
+
   /* START LOOP: for every article: */
 
   for(let article of articles){
 
     /* find tags wrapper */
 
-    const tagsWarpper = article.querySelectorAll('optArticleTagsSelector');
-    tagsWarpper.innerHTML = '';
-    console.log('tagsWarpper: ', tagsWarpper);
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    tagsWrapper.innerHTML = '';
+    console.log('tagsWrapper: ', tagsWrapper);
 
     /* make html variable with empty string */
 
@@ -143,7 +144,13 @@ function generateTags() {
 
       /* generate HTML of the link */
 
+      const tagsHTML = '<li><a href="#tag-'+ tag + '"><span>' + tag + '</span></a></li>';
+      console.log('tagsHTML: ', tagsHTML);
+
       /* add generated code to html variable */
+
+      html = html + ' ' + tagsHTML;
+      console.log('html: ', html);
 
     /* END LOOP: for each tag */
 
@@ -151,8 +158,19 @@ function generateTags() {
 
     /* insert HTML of all the links into the tags wrapper */
 
+    tagsWrapper.innerHTML = html;
+    console.log('tags html: ', html);
+
   /* END LOOP: for every article: */
+
   }
 }
 
 generateTags();
+
+const tagsLinks = document.querySelectorAll('.list .list-horizontal');
+console.log('tagsLinks: ', tagsLinks);
+
+for(let link of tagsLinks){
+  link.addEventListener('click', titleClickHandler);
+}
